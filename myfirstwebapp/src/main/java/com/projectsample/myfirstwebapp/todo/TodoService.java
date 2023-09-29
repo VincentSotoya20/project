2eebf4cf1020ef489c10aafea01094f3a0415775
TPDX-010 <TPDX-010@PC-TPDX007>
2023-09-29 15:52:44 +0800

    Third Commit

--- myfirstwebapp/src/main/java/com/projectsample/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/projectsample/myfirstwebapp/todo/TodoService.java
@@ -3,6 +3,7 @@ package com.projectsample.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
@@ -28,5 +29,11 @@ public class TodoService {
 	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
 		Todo todo = new Todo (++todosCount, username, description, targetDate, done);
 		todos.add(todo);
+	}
+	
+	public void deleteById(int id) {
+		
+		Predicate<? super Todo> predicate ;
+//		todos.removeIf(predicate);
 	}
 }
